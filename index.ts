f49c1d93f783119ac9c75a616016f1d032e77c86
Matt Barlow <83474131+mattbarlow-sg@users.noreply.github.com>
2024-01-30 12:20:46 -0500

    assume role

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
-const bucket = new aws.s3.Bucket("mybucket", {});
+const config = new pulumi.Config();
+
+const roleToAssumeARN = config.require("pulumi-testing:roleToAssumeARN");
+
+const provider = new aws.Provider("myprovider", {
+    assumeRole: {
+        roleArn: roleToAssumeARN,
+        sessionName: "PulumiSession",
+    },
+});
+
+const bucket = new aws.s3.Bucket("mybucket", {}, { provider: provider });
 
 export const bucketName = bucket.id;
